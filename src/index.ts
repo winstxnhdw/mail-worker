f077daba6d2dc0840b60c9f273057583d27f4acc
winstxnhdw <winstxnhdw@gmail.com>
2023-08-31 19:35:24 +0800

    refactor: reduce identation

--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,43 @@ import { get_mail_request } from '@/get_mail_request'
 import type { Environment } from '@/types'
 import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses'
 
+async function main(request: Request, environment: Environment): Promise<Response> {
+  const config = get_config(environment)
+  const mail_request = await get_mail_request(request)
+
+  if (mail_request === undefined) {
+    return new Response('Invalid request!', { status: 400 })
+  }
+
+  const message = {
+    Subject: { Data: mail_request.subject },
+    Body: { Html: { Data: mail_request.html } },
+  }
+
+  const mail = new SendEmailCommand({
+    Source: mail_request.from,
+    ReturnPath: mail_request.from,
+    Destination: { ToAddresses: mail_request.to },
+    Message: message,
+  })
+
+  const credentials = {
+    accessKeyId: config.AWS_ACCESS_KEY_ID,
+    secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
+  }
+
+  const client = new SESClient({
+    region: config.AWS_REGION,
+    credentials: credentials,
+  })
+
+  const send_mail = await client.send(mail).catch(console.error)
+
+  return send_mail === undefined
+    ? new Response('Failed to send email!', { status: 500 })
+    : new Response('Email sent!', { status: 200 })
+}
+
 export default {
-  async fetch(request: Request, environment: Environment): Promise<Response> {
-    const config = get_config(environment)
-    const mail_request = await get_mail_request(request)
-
-    if (mail_request === undefined) {
-      return new Response('Invalid request!', { status: 400 })
-    }
-
-    const mail = new SendEmailCommand({
-      Source: mail_request.from,
-      ReturnPath: mail_request.from,
-      Destination: { ToAddresses: mail_request.to },
-      Message: {
-        Subject: { Data: mail_request.subject },
-        Body: { Html: { Data: mail_request.html } },
-      },
-    })
-
-    const client = new SESClient({
-      region: config.AWS_REGION,
-      credentials: {
-        accessKeyId: config.AWS_ACCESS_KEY_ID,
-        secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
-      },
-    })
-
-    const send_mail = await client.send(mail).catch(console.error)
-
-    return send_mail === undefined
-      ? new Response('Failed to send email!', { status: 500 })
-      : new Response('Email sent!', { status: 200 })
-  },
+  fetch: main,
 }
