7022879d7455e2f3282b37087c7ff51273771d22
winstxnhdw <winstxnhdw@gmail.com>
2023-10-04 22:46:24 +0800

    fix: use `pnpm` for verification

--- scripts/verify_email.ts
+++ scripts/verify_email.ts
@@ -1,10 +1,11 @@
 import { get_config } from '@/config'
 import { SESClient, VerifyEmailIdentityCommand } from '@aws-sdk/client-ses'
-import { stdin, stdout } from 'process'
+import 'dotenv/config'
+import { env, stdin, stdout } from 'process'
 import { createInterface } from 'readline/promises'
 
 async function main() {
-  const config = get_config(Bun.env)
+  const config = get_config(env)
 
   const client = new SESClient({
     region: config.AWS_REGION,
@@ -13,6 +14,8 @@ async function main() {
       secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
     },
   })
+
+  console.log(config.AWS_ACCESS_KEY_ID)
 
   const email = await createInterface(stdin, stdout).question('[?] Email: ')
 
