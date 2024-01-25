817bfa74fd707ac93dbed0291a606234b53c8a37
Winston H. <56998716+winstxnhdw@users.noreply.github.com>
2024-01-25 19:41:16 +0000

    refactor: early throw if no email is provided

--- scripts/verify_email.ts
+++ scripts/verify_email.ts
@@ -2,21 +2,23 @@ import { get_config } from '@/config'
 import { SESClient, VerifyEmailIdentityCommand } from '@aws-sdk/client-ses'
 
 async function main() {
-  const config = get_config(Bun.env)
-
-  const client = new SESClient({
-    region: config.AWS_REGION,
-    credentials: {
-      accessKeyId: config.AWS_ACCESS_KEY_ID,
-      secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
-    },
-  })
-
   const email = prompt('[?] Email: ')
 
   if (!email) {
     throw new Error('Email is required!')
   }
+
+  const config = get_config(Bun.env)
+
+  const credentials = {
+    accessKeyId: config.AWS_ACCESS_KEY_ID,
+    secretAccessKey: config.AWS_SECRET_ACCESS_KEY
+  }
+
+  const client = new SESClient({
+    region: config.AWS_REGION,
+    credentials: credentials
+  })
 
   await client.send(new VerifyEmailIdentityCommand({ EmailAddress: email })).then(console.log)
 }
