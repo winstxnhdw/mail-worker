10d53dc4d78f2654e810eeaa8abf0fd6fd90e375
winstxnhdw <winstxnhdw@gmail.com>
2024-04-01 01:01:01 +0800

    perf: defer header retrieval

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,8 @@ import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses'
 
 async function main(request: Request, environment: Record<string, unknown>): Promise<Response> {
   const config = get_config(environment)
-  const request_token = request.headers.get('Authorization')
 
-  if (config.AUTH_TOKEN && request_token !== config.AUTH_TOKEN) {
+  if (config.AUTH_TOKEN && request.headers.get('Authorization') !== config.AUTH_TOKEN) {
     return new Response('Unauthorised! Please check your token in the request.', { status: 401 })
   }
 
